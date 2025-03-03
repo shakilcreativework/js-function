36a852f60273e1c2af90cabdfa02c18f1776671c
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-03 13:54:11 +0600

    function passed in parameter

---
+++ parameter.js
@@ -0,0 +1,28 @@
+function vaatKhao(){
+    console.log('hand wash kore aso');
+    console.log('boso');
+    console.log('plate e khabar nao');
+    console.log('akhon khaya suru koro');
+}
+
+// vaatKhao();
+
+
+function sqaure(number){
+    // console.log(number);
+    const borgo = number * number;
+    return borgo;
+}
+
+const givenNumber = sqaure(5);
+// console.log(givenNumber);
+
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    // console.log(sum);
+    return sum;
+}
+
+const addition = add(5, 75);
+console.log(addition);
