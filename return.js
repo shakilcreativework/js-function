193c47612e57ddadf286fea81781639f4645f82e
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-03 17:02:57 +0600

    function can return a value

---
+++ return.js
@@ -0,0 +1,17 @@
+function tenTimes(number){
+    const result = number * 10;
+    return result;
+}
+
+const givenNum = tenTimes(5);
+// console.log(givenNum);
+
+
+
+function cutHalf(number){
+    const half = number / 2;
+    return half;
+}
+
+const givenHalf = cutHalf(10);
+console.log(givenHalf);
