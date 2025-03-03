a9bcf8d9e3da937d35072bbc09c5f53317940a5b
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-03 12:44:05 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,22 @@
+
+const number = 45;
+
+function fanOffKor(){
+    console.log('bosa theke uthe dara');
+    console.log('walk towards the switch');
+}
+
+// call the function
+// fanOffKor();
+
+
+// function name(){}
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('add past');
+}
+
+// call the function
+brushTeeth();
+
+
